cd0b23b27a5917d2eefdd8a361794be82540c6a3
abdullahBilalAH <118999240+abdullahBilalAH@users.noreply.github.com>
2023-08-20 05:11:04 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+function isPalindrome(string) {
+    string = string.toLowerCase();
+    var str = "";
+    for (var c = 0; c < string.length; c++) {
+        if (terms(string[c]))
+            str += string[c];
+    }
+    var flag1 = 0;
+    var flag2 = str.length - 1;
+    while (flag1 < flag2) {
+        if (str[flag1] !== str[flag2])
+            return false;
+        flag1++;
+        flag2--;
+    }
+    return true;
+}
+function terms(char) {
+    if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122)
+        return true;
+    else
+        return false;
+}
