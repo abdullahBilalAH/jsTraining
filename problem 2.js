5c7770df2970c5fc0d50a20aa16073fecd2df4b5
abdullahBilalAH <118999240+abdullahBilalAH@users.noreply.github.com>
2023-08-22 07:32:11 +0300

    Add files via upload

---
+++ problem 2.js
@@ -0,0 +1,17 @@
+function checkPrime(num) {
+    if(num==2)
+        return true;
+    if(num<=1)
+        return false;
+    for(var c=2;c<num;c++)
+        if(num%c==0)
+            return false;
+    return true;
+}
+//test
+for(var c=-100;c<=100;c++){
+    if(checkPrime(c))
+        console.log(c+ "is prime");
+    else
+        console.log(c+ "is not prime");
+}
