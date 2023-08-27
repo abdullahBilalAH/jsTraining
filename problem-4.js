f185e5ccfac6e59c1d65acbc3c4ee6fc80b0ef1e
abdullahBilalAH <118999240+abdullahBilalAH@users.noreply.github.com>
2023-08-27 23:14:35 +0300

    Add files via upload

---
+++ problem-4.js
@@ -0,0 +1,19 @@
+const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
+const getUniqueValues = (arrOfNum) => {
+    let ar =[];
+    for(let c=0;c<arrOfNum.length;c++){
+        if(check(ar,arrOfNum[c]));
+        else
+        ar[ar.length]=arrOfNum[c];
+    }
+    return ar;
+};
+function check(ar,value){
+    for(let c=0;c<ar.length;c++){
+        if(ar[c]==value)
+            return true;
+    }
+    return false;
+}
+console.log(getUniqueValues(arrOfNum));// [1, 2, 4, 5, 6]
+//time complexity is n^2
