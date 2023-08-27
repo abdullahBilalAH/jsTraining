13115e97edc2ca45de9c09bfaf40db21327be9b7
abdullahBilalAH <118999240+abdullahBilalAH@users.noreply.github.com>
2023-08-27 23:33:54 +0300

    Update problem-4.js

--- problem-4.js
+++ problem-4.js
@@ -1,19 +1,18 @@
+/*I wanted to use the 'set', but when I asked GPT,
+it considered it as a built-in function. 
+It's more likely to be a built-in data structure, but just to be cautious,
+I wrote the code in this way.*/
 const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
 const getUniqueValues = (arrOfNum) => {
     let ar =[];
+    let check=[];
     for(let c=0;c<arrOfNum.length;c++){
-        if(check(ar,arrOfNum[c]));
-        else
+        if(check[arrOfNum[c]]==undefined){
         ar[ar.length]=arrOfNum[c];
+        check[arrOfNum[c]]=0;
+        }
     }
     return ar;
 };
-function check(ar,value){
-    for(let c=0;c<ar.length;c++){
-        if(ar[c]==value)
-            return true;
-    }
-    return false;
-}
 console.log(getUniqueValues(arrOfNum));// [1, 2, 4, 5, 6]
-//time complexity is O(n^2)
+//time complexity is O(n)
